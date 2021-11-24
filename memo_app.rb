609384ce19f699f5b51326cde30d86e3c06722eb
k-miyahara <k-miyahara@beingcorp.co.jp>
2021-11-24 12:43:36 +0900

    再々提出

--- memo_app.rb
+++ memo_app.rb
@@ -3,6 +3,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'securerandom'
 
 helpers do
   def h(text)
@@ -10,16 +11,22 @@ helpers do
   end
 end
 
-def read_memo
+def read_memos
   File.open('memos.json') { |f| JSON.load(f) }
 end
 
-def write_memo(hash)
-  File.open('memos.json', 'w') { |f| JSON.dump(hash, f) }
+def write_memo(memos)
+  File.open('memos.json', 'w') { |f| JSON.dump(memos, f) }
+end
+
+def select_memo
+  @id = params[:memo_id]
+  @memos = read_memos
+  @memo = @memos[@id]
 end
 
 get '/memos' do
-  @memos = read_memo
+  @memos = read_memos
   erb :top_memo
 end
 
@@ -28,42 +35,34 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  hash = read_memo
-  memo_id = Time.now.strftime("%Y%m%d%H%M%S")
-  hash[memo_id] = { 'title' => params[:title], 'content' => params[:content] }
-  write_memo(hash)
-  @memos = hash
+  memo_id = SecureRandom.hex(10)
+  @memos = read_memos
+  @memos[memo_id] = { 'title' => params[:title], 'content' => params[:content] }
+  write_memo(@memos)
   redirect '/memos'
 end
 
 get '/memos/:memo_id' do
-  hash = read_memo
-  @id = params[:memo_id]
-  @memo = hash[@id]
+  select_memo
   erb :show_memo
 end
 
 get '/memos/:memo_id/edit' do
-  hash = read_memo
-  @id = params[:memo_id]
-  @memo = hash[@id]
+  select_memo
   erb :edit_memo
 end
 
 patch '/memos/:memo_id' do
-  hash = read_memo
-  @id = params[:memo_id]
-  hash[@id]['title'] = params[:title]
-  hash[@id]['content'] = params[:content]
-  write_memo(hash)
-  @memo = hash[@id]
+  select_memo
+  @memo['title'] = params[:title]
+  @memo['content'] = params[:content]
+  write_memo(@memos)
   erb :show_memo
 end
 
 delete '/memos' do
-  hash = read_memo
-  hash.delete(params[:memo_id])
-  write_memo(hash)
-  @memos = hash
+  @memos = read_memos
+  @memos.delete(params[:memo_id])
+  write_memo(@memos)
   erb :top_memo
 end
